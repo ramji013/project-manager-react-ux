fa2b74e95905d9dccf3ff87e499a9bb15dec6915
Ram <ramprm@gmail.com>
2019-10-06 15:47:36 +0800

    controls added in user page

--- src/component/User.js
+++ src/component/User.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import '../App.css';
 import * as config from '../config/config';
-import {createData, getAllUser} from '../service/projectmanager';
+import {createData, updateData, deleteData} from '../service/projectmanager';
 import axios from 'axios';
 
 export default class User extends Component{
@@ -11,9 +11,10 @@ export default class User extends Component{
         lastName: "",
         employeeId: "",
         allUser: [],
-        editFirstName: "",
-        editLastName: "",
-        editEmployeeId: ""
+        isEditBtnClicked: false,
+        searchUser: "",
+        filteredData: [],
+        allUserBckup: []
     }
 
     componentDidMount(){
@@ -55,8 +56,9 @@ export default class User extends Component{
             "employeeId": employeeId
         }
         const response = createData(config.User_Url, userPayload)
-        allUser.push(userPayload);
-        this.setState({allUser: allUser})
+        if(response.data)
+            allUser.push(userPayload);
+        this.setState({allUser: allUser}) 
         console.log("response: "+ response)
     }
     }
@@ -67,16 +69,69 @@ export default class User extends Component{
         alert("user data: "+ userData)
         this.setState({firstName: userData.firstName,
             lastName: userData.lastName,
-            employeeId: userData.employeeId
+            employeeId: userData.employeeId,
+            isEditBtnClicked : true
         })
     }
 
+    deleteUser = (e) => {
+        const {allUser} = this.state;
+        allUser.splice(e.target.id,1)
+        this.setState({allUser})
+    }
+
+    updateUser = () => {
+        const {firstName, lastName, employeeId} = this.state
+        const payLoad = {
+            "firstName" : firstName,
+            "lastName" : lastName,
+            "employeeId" : employeeId
+        }
+    }
+
+    filterByFirstName = () => {
+        const {allUser,searchUser} = this.state;
+            this.setState({
+                allUserBckup: allUser,
+                allUser: allUser.filter((data) => {return data.firstName.startsWith(searchUser)})});
+    }
+
+    filterByLastName = () => {
+        const {allUser,searchUser} = this.state;
+            this.setState({
+                allUserBckup: allUser,
+                allUser: allUser.filter((data) => {return data.lastName.startsWith(searchUser)})});
+    }
+
+    filterById = () => {
+        const {allUser,searchUser} = this.state;
+            this.setState({
+                allUserBckup: allUser,
+                allUser: allUser.filter((data) => {return data.employeeId.startsWith(searchUser)})});
+    }
+
+    cancelUpdate = () => {
+        this.setState({
+            firstName: "",
+            lastName: "",
+            employeeId: "",
+            isEditBtnClicked: false})
+    }
+
+    updateSearchStateChange = (e) => {
+        const {allUserBckup} = this.state;
+        if(!e.target.value && allUserBckup.length>0){
+            this.setState({allUser: allUserBckup})
+        }
+        this.setState({searchUser: e.target.value})
+    }
 
     render(){
-        const {firstName, lastName, employeeId,searchUser,allUser,editFirstName, editLastName, editEmployeeId} = this.state;
+        const {firstName, lastName, employeeId,searchUser,allUser,isEditBtnClicked} = this.state;
         return(
             <div>
             <div className="align-center">
+                <form>
                 <table>
                     <tbody>
                         <tr><td><label htmlFor="firstName">First Name:</label></td><td><input name="firstName" type="text" value={firstName} onChange={this.updateFirstName} required></input></td></tr> 
@@ -84,31 +139,39 @@ export default class User extends Component{
                         <tr><td><label htmlFor="employeeId">Employee ID:</label></td><td><input name="employeeId" type="text" value= {employeeId} onChange={this.updateEmployeeId} required></input></td></tr> 
                     </tbody>
                 </table>
-                <div className="Container">
+                </form>
+                {
+                isEditBtnClicked===false ?
+                <div className="container">
                     <button onClick={this.createUser}> Add </button>
                     <button onClick= {this.reset} className="reset"> Reset </button>
+                </div>:
+                <div className="container">
+                    <button onClick={this.updateUser}> Update </button>
+                    <button onClick= {this.cancelUpdate}> Cancel </button>
                 </div>
+                }
                 <hr className="boder-solid"/>
-                <div className="Container">
-                    <input type="text" name="search" placeholder="search" value={searchUser}></input>
+                <div className="container">
+                    <input type="text" name="search" placeholder="search" value={searchUser} onChange={this.updateSearchStateChange}></input>
                     <label>Sort:</label>
-                    <button type="button" onClick={this.sortByFirstName}>First Name</button>
-                    <button type="button" onClick={this.sortByLastName}>Last Name</button>
-                    <button type="button" onClick={this.sortById}>Id</button>
+                    <button type="button" onClick={this.filterByFirstName}>First Name</button>
+                    <button type="button" onClick={this.filterByLastName}>Last Name</button>
+                    <button type="button" onClick={this.filterById}>Id</button>
                 </div>
                 <hr className="boder-dotted"/>
 
                         {
                             allUser.map((data, idx) => (
-                                <div id={idx} className="row">
+                                <div id={data.employeeId} className="row">
                                     <div className="column">
-                                        First Name:<label value={data.fistName}>{data.firstName}</label><br/>
+                                        First Name: <label value={data.fistName}>{data.firstName}</label><br/>
                                         Last Name: <label value={data.lastName}>{data.lastName}</label><br/>
                                         Employee Id: <label value={data.employeeId}>{data.employeeId}</label>
                                     </div>
                                     <div className="column">
                                         <div><button onClick={this.editUser} id={idx}>Edit</button></div>
-                                        <div><button onClick={this.deleteUser}>Delete</button></div>
+                                        <div><button onClick={this.deleteUser} id={idx}>Delete</button></div>
                                     </div>
                                     <hr className="boder-dotted"/>
                                 </div>
--- src/service/projectmanager.js
+++ src/service/projectmanager.js
@@ -8,9 +8,25 @@ export const createData = (url, payload) => {
     })
 }
 
-export const getAllUser = (url) => {
+export const updateData = (url, payload) => {
+    axios.put(url, payload).then(response => {
+            return response.data
+    }).catch(error => {
+        console.log("exception occured while saving data: for the url " + url+ " exception: "+ error)
+    })
+}
+
+export const getAll = (url) => {
     axios.get(url).then(response => {
         return response.data
+    }).catch(error => {
+        console.log("exception occured while saving data: for the url " + url+ " exception: "+ error)
+    })
+}
+
+export const deleteData = (url) => {
+    axios.delete(url).then(response => {
+            return response.data
     }).catch(error => {
         console.log("exception occured while saving data: for the url " + url+ " exception: "+ error)
     })
